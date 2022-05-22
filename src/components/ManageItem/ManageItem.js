fecab05c58f5ac8e61e901c472252f6e5636de94
Tanvir3744 <biplobmohammad1@gmail.com>
2022-05-22 22:28:43 +0600

    handle delete complete

--- src/components/ManageItem/ManageItem.js
+++ src/components/ManageItem/ManageItem.js
@@ -11,47 +11,66 @@ const ManageItem = () => {
             .then(data => setManageProduct(data))
     }, [])
 
-    const handleDeleteItem = () => {
-        console.log('this is product is deleting by the user')
+    const handleDeleteItem = (id) => {
+        let proceedDelete = window.confirm('Want to delete this item?')
+
+        //handle deleting through api
+        if (proceedDelete) {
+            const url = `https://peaceful-plains-32871.herokuapp.com/allItems/${id}`;
+            fetch(url, {
+                method: "DELETE",
+                headers: {
+                    'content-type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        const afterDeletion = manageProduct.filter(product => product._id !== id);
+                        setManageProduct(afterDeletion)
+                    }
+                })
+        }
+
     }
-return (
-    <div className='container mt-5'>
-        <h2 className='mt-2'>Manage Your Item From Here</h2>
-        <div className="row">
-            <div className="col-lg-10 w-100 mx-auto">
-                <MDBTable responsive bordered borderColor="primary" >
-                    <MDBTableHead>
-                        <tr>
-                            <th scope='col'>Name</th>
-                            <th scope='col'>Price</th>
-                            <th scope='col'>Supplier</th>
-                            <th scope='col'>Available Quantity</th>
-                            <th scope='col'>Manage</th>
-                        </tr>
-                    </MDBTableHead>
-                    <MDBTableBody>
-                        {
-                            manageProduct.map(product =>
-                                <tr>
-                                    <th scope='row'>{product.name}</th>
-                                    <td>{product.price}</td>
-                                    <td>{product.supplier}</td>
-                                    <td>{product.quantity}</td>
-                                    <td>
-                                        <div>
-                                            <MDBBtn color='danger' className='ripple mx-2'>Delete</MDBBtn>
-                                            <Link to='/update'  className='ripple btn btn-success'>Update</Link>
-                                        </div>
-                                    </td>
-                                </tr>
-                            )
-                        }
-                    </MDBTableBody>
-                </MDBTable>
+    return (
+        <div className='container mt-5'>
+            <h2 className='mt-2'>Manage Your Item From Here</h2>
+            <div className="row">
+                <div className="col-lg-10 w-100 mx-auto">
+                    <MDBTable responsive bordered borderColor="primary" >
+                        <MDBTableHead>
+                            <tr>
+                                <th scope='col'>Name</th>
+                                <th scope='col'>Price</th>
+                                <th scope='col'>Supplier</th>
+                                <th scope='col'>Available Quantity</th>
+                                <th scope='col'>Manage</th>
+                            </tr>
+                        </MDBTableHead>
+                        <MDBTableBody>
+                            {
+                                manageProduct.map(product =>
+                                    <tr>
+                                        <th scope='row'>{product.name}</th>
+                                        <td>{product.price}</td>
+                                        <td>{product.supplier}</td>
+                                        <td>{product.quantity}</td>
+                                        <td>
+                                            <div>
+                                                <MDBBtn onClick={() => handleDeleteItem(product._id)} color='danger' className='ripple mx-2'>Delete</MDBBtn>
+                                                <Link to='/update' className='ripple btn btn-success'>Update</Link>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            }
+                        </MDBTableBody>
+                    </MDBTable>
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
 };
 
 export default ManageItem;
